8c66e969c05ca25cfb0072b9d6dab5a2cb5b199e
ROSAAREBALO <rousetemari@gmail.com>
2019-04-06 20:15:18 -0200

    pruebas a los metodos http en la carpeta ficture

--- routes/web.php
+++ routes/web.php
@@ -10,46 +10,38 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::get('/esta-url', function(){
       return 'HOLA MUNDO';
 });
-
 Route::get('/usuarios/detalles/{id}', function($id){
         return 'Detalles de usuario: ' .$id;
 });
  Route::get('/posts/{posts_id}/coment/{coment_id}', function($postsId,$comentId){
          return "este es el comentario $comentId y su post de $postsId " ;
- 
  });
-
  Route::get('/user', function(){
       return "food";
  });
-
  Route::post('/user', function(){
       return "bat";
  });
-
- Route::get('users/{id}',function($id){
+ Route::get('/userser/{id}',function($id){
     return "el id es : $id";
- })->where(['id'=> '[\d]+' ]);
-
- Route::get('users/{name}',function($name){
+ })->where(['id'=> '[0-9]+' ]);
+ Route::get('/users/{name}',function($name){
     return "el nombre es : $name";
- })->where(['name'=>'[-\w]+']);
-
- Route::get('saludo/{name}/{apodo?}',function($name,$apodo=null){
+ })->where(['name'=>'[A-Z]+']);
+ Route::get('/saludo/{name}/{apodo?}',function($name,$apodo=null){
        if($apodo){
 	      return "tengo nombre $name y tengo apodo $apodo";
 	   }else{
 	       return "tengo nombre $name y no tengo apodo";
 	   }
- 
  });
+Route::get('/detalle/{id}', function($id){
+  return "detalle con id $id";
+});
 
